22ab43271ab3cc28b5c22d850ba594bf2f691d65
KantaSwift <kanta-_-soccer.love@docomo.ne.jp>
2023-01-30 22:41:39 +0900

    first commit

---
+++ SwiftConcurrencyGithub/SwiftConcurrencyGithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftConcurrencyGithubApp.swift
+//  SwiftConcurrencyGithub
+//
+//  Created by 上條栞汰 on 2023/01/30.
+//
+
+import SwiftUI
+
+@main
+struct SwiftConcurrencyGithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
